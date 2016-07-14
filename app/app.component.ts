9132ef68b216df969f735731711ed62d960f5b45
ravkovskiy <kos1992@mail.ru>
2016-07-14 16:09:45 +0300

    add layout-page

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    templateUrl: 'app/app.template.html'
 })
 export class AppComponent { }
