03740724ad31656fcb21c6ed154a92a4706425df
ravkovskiy <kos1992@mail.ru>
2016-07-19 20:53:30 +0300

    опробован другой способ загрузки пользовательских файлов

--- server/server.ts
+++ server/server.ts
@@ -75,3 +75,25 @@ var server = app.listen(port, function() {
 });
 
 
+
+
+var fs = require('fs');
+var busboy = require('connect-busboy');
+
+app.use(busboy()); 
+
+app.post('/fileupload', function(req, res) {
+    console.log('ok');
+    var fstream;
+    req.pipe(req.busboy);
+    req.busboy.on('file', function (fieldname, file, filename) {
+        console.log("Uploading: " + filename); 
+        fstream = fs.createWriteStream(__dirname + '/files/' + filename);
+        file.pipe(fstream);
+        fstream.on('close', function () {
+            res.redirect('back');
+        });
+    });
+});
+
+
